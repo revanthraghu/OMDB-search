3c4b8cd663bdaa9f2a2469a8c6bbe9fced47229e
Revanth Raghu <revabob@gmail.com>
2020-06-06 16:36:58 +0530

    added displayResults function

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-function setUrlParams() {
-    var search_term = document.querySelector('#search').value;
+//function to add api request parameters to url
+function setUrlParams(search_term) {
+
+    //exrtact the search type radio button value
     var search_type_form = new FormData(document.querySelector('.search-types'))
     var search_type;
     for(var value of search_type_form.values()) {
         search_type = value
     }
+
+    //create new URL object instance and add params to url
     var url  = new URL("http://www.omdbapi.com/?apikey=edccade0")
-    url.searchParams.append("s", search_term)
-    url.searchParams.append("type", search_term)
+    if(search_type !== 'title') {
+        url.searchParams.append("s", search_term)
+    } else {
+        url.searchParams.append("t", search_term)
+    }
+    if(search_type == 'movie' || search_type == 'series') {
+        url.searchParams.append("type", search_type)
+    }
+    return url
 }
 
-// //function to genrate divs of result items
+// //function to genrate card of result items
 // function displayResultItems(result_items_div, result_items) {
 // // display results as divs with paras
 // // from data extract full_name, html_url, description, language, license.name, pushed_at
@@ -56,42 +67,63 @@ function setUrlParams() {
 //     })
 // }
 
-// function displayResults(search_results) {
-//     var results_div = document.querySelector('.results')
-//     if(results_div.children.length !== 0 && url.searchParams.has("sort")) {
-//         while (results_div.children.length > 1) {
-//             results_div.removeChild(results_div.lastChild)
-//         }
-//     document.querySelector(".head > h2").textContent = search_results.total_count + " repository results"    
-//     }
-//     else {
-//         while (results_div.children.length > 0) {
-//             results_div.removeChild(results_div.lastChild)
-//         }
-//     }
-//     var result_items_div = document.createElement('div')
-//     result_items_div.setAttribute('style', 'display: flex; margin: 2% 20% 2% 20%; flex-direction: column')
-//     //call function to generate cards
-//     displayResultItems(result_items_div, search_results.items)
-//     results_div.append(result_items_div)
+function displayResults(response_time, search_results) {
 
-// }
+    //get results div from DOM
+    var results_div = document.querySelector('.results')
 
-// function getSearchResults(url) {
-//     var results = new XMLHttpRequest()
-//     results.open("GET", url)
-//     results.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
-//     results.send()
-//     results.onload = function() {
-//         var search_results = JSON.parse(results.response)
-//         displayResults(search_results)
-//     }
-// }
+    //remove all results from previous search
+    if(results_div.children.length !== 0) {
+        while (results_div.children.length > 0) {
+            results_div.removeChild(results_div.lastChild)
+        }
+    }
 
+    //Check if results obtained from API and display response
+    if(search_results.Response === 'False') {
+        var error_msg = document.createElement('h2')
+        error_msg.textContent = search_results.Error + ' Try another name.'
+        error_msg.style.textAlign = 'center'
+        results_div.appendChild(error_msg)
+    }
+    else {
+        var result_count = document.createElement('div')
+        result_count.textContent = 'Found ' + search_results.totalResults + ' results in ' + response_time + 's'
+        result_count.style.color = 'gray'
+        var hr = document.createElement('hr')
+        results_div.append(result_count, hr)
+        displayResultItems(search_results.Search, results_div)
+    }
+
+}
+
+//main function that runs on submitting search term
+function getSearchResults(search_term) {
+    var url = setUrlParams(search_term)
+    var results = new XMLHttpRequest()
+    results.open("GET", url)
+    results.send()
+    var request_time = (new Date()).getTime()
+    results.onload = function() {
+        var search_results = JSON.parse(results.response)
+        var response_time = ((new Date()).getTime() - request_time)/1000
+        displayResults(response_time, search_results)
+        //per page 10 items displayed...do pagination
+    }
+}
+
+//Submit event listener for the search-bar form
 document.querySelector('form').addEventListener('submit', function (submit_event) {
+
+    //prevent default action of submit button
     submit_event.preventDefault();
-    setUrlParams()
+
+    //get the search string from search bar
+    var search_term = document.querySelector('#search').value;
+
+    //Change the webpage title to show the search term
     document.querySelector('title').textContent = "Search - " + search_term
-        //setUrlParams(search_term, )
-        //getSearchResults(url)
+
+    //display results for the seearch term
+    getSearchResults(search_term)
 })
